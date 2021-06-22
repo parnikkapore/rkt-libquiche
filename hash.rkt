a9be8395020ca620ee0c73cc1bbf04e734e6ba33
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2021-06-22 09:15:21 +0700

    Added test for hash.rkt

--- hash.rkt
+++ hash.rkt
@@ -14,4 +14,21 @@
       hash
       (hash-path (hash-ref hash (first keys)) (rest keys))))
 
-(module+ test )
+(module+ test
+  (define test-hash
+    #hash((Languages .
+                     #hash((ALGOL .
+                                  #hash((BASIC . "c64emu")
+                                        (C . "gcc")
+                                        (C++ . "g++")))
+                           (Lisp .
+                                 #hash(("Common Lisp" . "gnucl")
+                                       ("Emacs Lisp" . "emacs")
+                                       (Racket . "racket")))))))
+  
+  (check-equal? (hash-path test-hash '(Languages ALGOL C++))
+                "g++")
+  (check-equal? (hash-path test-hash '(Languages Lisp "Emacs Lisp"))
+                "emacs")
+  (check-equal? (hash-path test-hash '(Languages ALGOL))
+                #hash((BASIC . "c64emu") (C . "gcc") (C++ . "g++"))))
